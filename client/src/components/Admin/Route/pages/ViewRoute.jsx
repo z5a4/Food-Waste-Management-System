197a007027673f7573317f97402d8459c3e654da
MonsterZero001 <153346230+MonsterZero001@users.noreply.github.com>
2024-04-15 16:39:05 +0530

    Commit

--- client/src/components/Admin/Route/pages/ViewRoute.jsx
+++ client/src/components/Admin/Route/pages/ViewRoute.jsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import Footer from "../../../Footer/Footer";
 import FViewRoute from '../functions/FViewRoute';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button } from '@material-tailwind/react';
+import { Button, Typography } from '@material-tailwind/react';
 
 const ViewRoute = () => {
   const [routes, setRoutes] = useState([]);
@@ -22,8 +23,9 @@ const ViewRoute = () => {
   }, [fetchRoutes]);
 
   return (
+    <>
     <div className="container mt-5 pt-5">
-      <h1 className="text-center">Routes</h1>
+      <Typography variant='h3' className="text-center mb-3">Routes</Typography>
       <div className="flex justify-center mb-3">
           <Link to="/adminrouteform">
             <Button className='mr-2' color="green" >
@@ -46,10 +48,8 @@ const ViewRoute = () => {
             <th className="px-4 py-2">Route Landmark</th>
             <th className="px-4 py-2">Transporter ID</th>
             <th className="px-4 py-2">Driver Name</th>
-            <th className="px-4 py-2">Mobile No</th>
             <th className="px-4 py-2">Volunteer ID</th>
             <th className="px-4 py-2">Volunteer Name</th>
-            <th className="px-4 py-2">Volunteer Mobile No</th>
             <th className="px-4 py-2">Actions</th>
           </tr>
         </thead>
@@ -62,10 +62,8 @@ const ViewRoute = () => {
               <td className="border px-4 py-2">{route.routeLandmark}</td>
               <td className="border px-4 py-2">{route.transporterId}</td>
               <td className="border px-4 py-2">{route.transporterdriveName}</td>
-              <td className="border px-4 py-2">{route.transportermobileNo}</td>
               <td className="border px-4 py-2">{route.volunteerId}</td>
               <td className="border px-4 py-2">{route.volunteerName}</td>
-              <td className="border px-4 py-2">{route.volunteermobileNo}</td>
               <td className="border px-4 py-2">
                 <div className="flex justify-center">
                   <Button type="button" color="green" size="sm" className="me-2" onClick={() => handleUpdate(route)}>Edit</Button>
@@ -77,6 +75,8 @@ const ViewRoute = () => {
         </tbody>
       </table>
     </div>
+    <Footer/>
+    </>
   );
 };
 
